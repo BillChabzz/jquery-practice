bc9fb3e6c6b8cc88e797e284631a8a861766de48
BillChabzz <chaby9.8@gmail.com>
2015-10-04 06:46:28 +0300

    learning more functions

--- js/jquerryc.js
+++ js/jquerryc.js
@@ -22,3 +22,8 @@ $('div').children('h2').children('a');
  A better way to get the anchor tag using find method
  */
 $('div').find('a');
+
+$('h2').next(); //selects the next sibling (the paragraph)/
+$('h2').nextAll(); //selects all the siblings after it
+$('p').prev(); //selects the previous sibling (the h2)/
+$('p').prevAll(); //selects the previous siblings

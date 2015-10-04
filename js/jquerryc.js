ad1eab7ca9b75facc53d68c812325e09ba5acaba
BillChabzz <chaby9.8@gmail.com>
2015-10-04 06:28:20 +0300

    added project files

---
+++ js/jquerryc.js
@@ -0,0 +1,3 @@
+$('#something').animate({'margin-top': '300px', 'margin-left': '400px'},1000);
+$('h2').parent(); //this would select the div
+$('h2').children('a'); //selects all children of the h2 that are anchor elements

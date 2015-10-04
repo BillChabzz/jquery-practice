337c6fd0b59e5b407dd7d4bebd225de0bf23da0f
BillChabzz <chaby9.8@gmail.com>
2015-10-04 06:35:14 +0300

    more learning

--- js/jquerryc.js
+++ js/jquerryc.js
@@ -1,3 +1,10 @@
 $('#something').animate({'margin-top': '300px', 'margin-left': '400px'},1000);
+//animate({'property':'value'},speed,callback);
+
 $('h2').parent(); //this would select the div
 $('h2').children('a'); //selects all children of the h2 that are anchor elements
+$('li'); //selects all list items
+$('li').children(); //selects list children
+$('li').eq(2); //selects list item 3
+//jquery principle of chainability...running functions after another like below
+$('ul').children('li').eq(3).animate({...});
